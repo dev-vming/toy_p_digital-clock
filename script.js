3288ae7348479158294bfb29123a4937e8ae8611
dev-vming <dev.vming@gmail.com>
2023-09-11 21:57:40 +0900

    디지털시계 초안

---
+++ script.js
@@ -0,0 +1,33 @@
+// ID를 통해 변수 정의
+const todayDiv = document.querySelector('#today');
+const timeDiv = document.querySelector('#time');
+
+// 날짜와 시간 불러오는 함수 생성
+function getTime() {
+    let now = new Date();
+
+    // 메서드 이용해 날짜와 시간 호출 (이때 getMonth는 0월부터 표시되므로 +1)
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    // 삼항연산자 사용하여 숫자가 두자리로 나오게끔 처리
+    month = month < 10 ? `0${month}` : month;
+    date = date < 10 ? `0${date}` : date;
+    hour = hour < 10 ? `0${hour}` : hour;
+    minute = minute < 10 ? `0${minute}` : minute;
+    second = second < 10 ? `0${second}` : second;
+
+
+    // 템플릿 리터럴 이용하여 날짜와 시간 표시
+    todayDiv.textContent = `${year}년 ${month}월 ${date}일`;
+    timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
+}
+
+// setInterval로 인해 페이지가 로드 된 후 1초간 시계 보이지 않는 현상 개선
+getTime();
+
+setInterval(getTime, 1000);
